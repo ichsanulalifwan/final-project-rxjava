27f3f5a8073232f70482fb7523d88b230e54cb8d
ichsanulalifwan <ichsanulalifwan@gmail.com>
2023-05-26 23:49:39 +0800

    add location

---
+++ core/src/main/java/com/app/ichsanulalifwan/barani/core/data/LocationRepository.kt
@@ -0,0 +1,25 @@
+package com.app.ichsanulalifwan.barani.core.data
+
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import java.util.*
+
+class LocationRepository(context: Context, val isMock: Boolean) {
+
+    private val geoCoder = Geocoder(context)
+
+    fun getAddresses(
+        longitude: Double,
+        latitude: Double, 
+        maxResults: Int,
+        block: (List<Address>?, Throwable?) -> Unit
+    ) {
+        try {
+            val mockAddress = Address(Locale.US).apply { countryCode = "US" }
+            block(if (isMock) listOf(mockAddress) else geoCoder.getFromLocation(latitude, longitude, maxResults), null)
+        } catch (t: Throwable) {
+            block(null, t)
+        }
+    }
+}
