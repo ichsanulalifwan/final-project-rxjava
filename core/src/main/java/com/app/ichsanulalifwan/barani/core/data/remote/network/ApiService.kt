cdc95fc7e8cdb268c49c0bd8a5bf0b9052b12279
ichsanulalifwan <ichsanulalifwan@gmail.com>
2023-10-22 16:52:50 +0800

    update responses

--- core/src/main/java/com/app/ichsanulalifwan/barani/core/data/remote/network/ApiService.kt
+++ core/src/main/java/com/app/ichsanulalifwan/barani/core/data/remote/network/ApiService.kt
@@ -1,16 +1,28 @@
 package com.app.ichsanulalifwan.barani.core.data.remote.network
 
-import com.app.ichsanulalifwan.barani.core.model.NewsResponse
+import com.app.ichsanulalifwan.barani.core.data.remote.response.NewsResponse
+import com.app.ichsanulalifwan.barani.core.data.remote.response.PublisherResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("top-headlines")
-    fun getNews(
+    @GET("v2/top-headlines")
+    fun getTopHeadlines(
         @Query("country") country: String,
         @Query("category") category: String,
+        @Query("apiKey") apiKey: String
+    ): Call<NewsResponse>
+
+    @GET("v2/top-headlines/sources")
+    fun getNewsPublishers(
+        @Query("apiKey") apiKey: String
+    ): Call<PublisherResponse>
+
+    @GET("v2/everything")
+    fun getEverything(
+        @Query("q") country: String,
         @Query("apiKey") apiKey: String
     ): Call<NewsResponse>
 }
---
+++ core/src/main/java/com/app/ichsanulalifwan/barani/core/data/remote/response/NewsResponse.kt
@@ -0,0 +1,15 @@
+package com.app.ichsanulalifwan.barani.core.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class NewsResponse(
+
+	@SerializedName("totalResults")
+	val totalResults: Int? = 0,
+
+	@SerializedName("articles")
+	val articles: List<ArticlesItemResponse>,
+
+	@SerializedName("status")
+	val status: String? = "",
+)
