698bfe005448bf3af8d02b9ef508b503a8a21871
ichsanulalifwan <ichsanulalifwan@gmail.com>
2023-10-29 14:04:28 +0800

    implement Publisher Entity in Dao

--- core/src/main/java/com/app/ichsanulalifwan/barani/core/data/source/local/room/NewsDao.kt
+++ core/src/main/java/com/app/ichsanulalifwan/barani/core/data/source/local/room/NewsDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.app.ichsanulalifwan.barani.core.data.source.local.entity.NewsEntity
+import com.app.ichsanulalifwan.barani.core.data.source.local.entity.PublisherEntity
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Single
@@ -16,17 +17,29 @@ interface NewsDao {
      * Load data.
      */
     @Query("SELECT * FROM news_entities")
-    fun allByFlowable(): Flowable<List<NewsEntity>>
+    fun allNewsByFlowable(): Flowable<List<NewsEntity>>
 
     @Query("SELECT * FROM news_entities")
-    fun allBySingle(): Single<List<NewsEntity>>
+    fun allNewsBySingle(): Single<List<NewsEntity>>
+
+    @Query("SELECT * FROM publisher_entities")
+    fun allPublisherByFlowable(): Flowable<List<PublisherEntity>>
+
+    @Query("SELECT * FROM publisher_entities")
+    fun allPublisherBySingle(): Single<List<PublisherEntity>>
 
     /**
      * Insert data.
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAsCompletable(newsEntities: List<NewsEntity>): Completable
+    fun insertNewsAsCompletable(newsEntities: List<NewsEntity>): Completable
 
     @Query("DELETE FROM news_entities")
-    fun deleteAsCompletable() : Completable
+    fun deleteNewsAsCompletable() : Completable
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPublisherAsCompletable(publisherEntities: List<PublisherEntity>): Completable
+
+    @Query("DELETE FROM publisher_entities")
+    fun deletePublisherAsCompletable() : Completable
 }
