7da5b0a0fc3583d59e73d82849bbc3aa927b6eb2
ichsanulalifwan <ichsanulalifwan@gmail.com>
2023-10-31 14:19:36 +0800

    fix news fetch data

--- app/src/main/java/com/app/ichsanulalifwan/barani/ui/beranda/BerandaViewModel.kt
+++ app/src/main/java/com/app/ichsanulalifwan/barani/ui/beranda/BerandaViewModel.kt
@@ -1,18 +1,28 @@
 package com.app.ichsanulalifwan.barani.ui.beranda
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.toLiveData
+import com.app.ichsanulalifwan.barani.core.R
 import com.app.ichsanulalifwan.barani.core.data.repository.news.NewsRepository
 import com.app.ichsanulalifwan.barani.core.model.News
 import com.app.ichsanulalifwan.barani.core.utils.DataMapper
 import com.app.ichsanulalifwan.barani.core.viewmodel.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 class BerandaViewModel(
     application: Application,
     private val newsRepository: NewsRepository,
 ) : BaseViewModel(application) {
+
+    private val disposableBag = CompositeDisposable()
+
+    init {
+        getTopHeadlineNews()
+    }
 
     override fun getNews(): LiveData<List<News>> = newsRepository.news
         .map { entityList ->
@@ -22,7 +32,22 @@ class BerandaViewModel(
         .toLiveData()
 
     override fun getTopHeadlineNews() {
-        TODO("Not yet implemented")
+        startTopHeadlinesNewsTimer()
+        newsRepository.getTopHeadlineNews("us", "health")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { isLoading.value = true }
+            .doFinally {
+                stopTopHeadlinesNewsTimer()
+                isLoading.value = false
+            }
+            .subscribe(
+                { isLocalNews.value = false },
+                { throwable ->
+                    message.value = context.getString(R.string.news_error)
+                    Log.e(LOG_TAG, "Could not fetch news", throwable)
+                }
+            ).also { disposableBag.add(it) }
     }
 
     override fun getNewsPublisher() {
--- app/src/main/java/com/app/ichsanulalifwan/barani/ui/list/ListViewModel.kt
+++ app/src/main/java/com/app/ichsanulalifwan/barani/ui/list/ListViewModel.kt
@@ -1,18 +1,24 @@
 package com.app.ichsanulalifwan.barani.ui.list
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.toLiveData
+import com.app.ichsanulalifwan.barani.core.R
 import com.app.ichsanulalifwan.barani.core.data.repository.news.NewsRepository
 import com.app.ichsanulalifwan.barani.core.model.News
 import com.app.ichsanulalifwan.barani.core.utils.DataMapper
 import com.app.ichsanulalifwan.barani.core.viewmodel.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 class ListViewModel(
     application: Application,
     private val newsRepository: NewsRepository,
 ) : BaseViewModel(application) {
+
+    private val disposableBag = CompositeDisposable()
 
     override fun getNews(): LiveData<List<News>> = newsRepository.news
         .map { entityList ->
@@ -22,7 +28,22 @@ class ListViewModel(
         .toLiveData()
 
     override fun getTopHeadlineNews() {
-        TODO("Not yet implemented")
+        startTopHeadlinesNewsTimer()
+        newsRepository.getTopHeadlineNews("us", "health")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { isLoading.value = true }
+            .doFinally {
+                stopTopHeadlinesNewsTimer()
+                isLoading.value = false
+            }
+            .subscribe(
+                { isLocalNews.value = false },
+                { throwable ->
+                    message.value = context.getString(R.string.news_error)
+                    Log.e(LOG_TAG, "Could not fetch news", throwable)
+                }
+            ).also { disposableBag.add(it) }
     }
 
     override fun getNewsPublisher() {
