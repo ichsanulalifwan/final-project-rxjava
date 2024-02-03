1cc2bfecf961e7df09a66ec507e0084315ad4e2a
ichsanul.alifwan <ichsanulalifwan@gmail.com>
2024-02-03 16:03:03 +0800

    add benchmark file

---
+++ benchmark/src/main/java/com/app/ichsanulalifwan/barani/benchmark/IntegrationBenchmark.kt
@@ -0,0 +1,20 @@
+package com.app.ichsanulalifwan.barani.benchmark
+
+import org.junit.Test
+
+interface IntegrationBenchmark {
+    @Test
+    fun integration_1()
+
+    @Test
+    fun integration_2()
+
+    @Test
+    fun integration_3()
+
+    @Test
+    fun integration_4()
+
+    @Test
+    fun integration_reactive()
+}
