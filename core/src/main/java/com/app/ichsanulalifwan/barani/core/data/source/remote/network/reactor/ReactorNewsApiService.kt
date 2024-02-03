c30d5102e0d1bc776f5dcf5027137a30edc9582b
ichsanul.alifwan <ichsanulalifwan@gmail.com>
2024-02-04 03:42:33 +0800

    implement RxJavaNewsApiService, FlowNewsApiService, ReactorNewsApiService

--- core/src/main/java/com/app/ichsanulalifwan/barani/core/data/source/remote/network/reactor/ReactorNewsApiService.kt
+++ core/src/main/java/com/app/ichsanulalifwan/barani/core/data/source/remote/network/reactor/ReactorNewsApiService.kt
@@ -1,29 +1,29 @@
-package com.app.ichsanulalifwan.barani.core.data.source.remote.network
+package com.app.ichsanulalifwan.barani.core.data.source.remote.network.reactor
 
 import com.app.ichsanulalifwan.barani.core.data.source.remote.response.NewsResponse
 import com.app.ichsanulalifwan.barani.core.data.source.remote.response.PublisherResponse
 import com.app.ichsanulalifwan.barani.core.utils.Constant
-import io.reactivex.Single
+import reactor.core.publisher.Mono
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface ApiService {
+interface ReactorNewsApiService {
 
     @GET("v2/top-headlines")
     fun getTopHeadlines(
         @Query("country") country: String,
         @Query("category") category: String,
-        @Query("apiKey") apiKey: String = Constant.NEWS_API_KEY,
-    ): Single<NewsResponse>
+        @Query("apiKey") apiKey: String = Constant.NEWS_API_KEY
+    ): Mono<NewsResponse>
 
     @GET("v2/top-headlines/sources")
     fun getNewsPublishers(
-        @Query("apiKey") apiKey: String = Constant.NEWS_API_KEY,
-    ): Single<PublisherResponse>
+        @Query("apiKey") apiKey: String = Constant.NEWS_API_KEY
+    ): Mono<PublisherResponse>
 
     @GET("v2/everything")
     fun getEverything(
         @Query("q") country: String,
-        @Query("apiKey") apiKey: String = Constant.NEWS_API_KEY,
-    ): Single<NewsResponse>
+        @Query("apiKey") apiKey: String = Constant.NEWS_API_KEY
+    ): Mono<NewsResponse>
 }
