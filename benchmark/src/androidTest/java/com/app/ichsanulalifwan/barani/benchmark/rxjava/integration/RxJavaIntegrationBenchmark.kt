afd677776fa4569fc28592fafe216cf71cc3d038
ichsanul.alifwan <ichsanulalifwan@gmail.com>
2024-02-06 02:11:18 +0800

    implement rxjava integration benchmark

--- benchmark/src/androidTest/java/com/app/ichsanulalifwan/barani/benchmark/rxjava/integration/RxJavaIntegrationBenchmark.kt
+++ benchmark/src/androidTest/java/com/app/ichsanulalifwan/barani/benchmark/rxjava/integration/RxJavaIntegrationBenchmark.kt
@@ -1,100 +1,104 @@
 package com.app.ichsanulalifwan.barani.benchmark.rxjava.integration
 
+import android.annotation.SuppressLint
+import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.app.ichsanulalifwan.barani.benchmark.IntegrationBenchmark
+import com.app.ichsanulalifwan.barani.benchmark.api.IntegrationBenchmark
 import com.app.ichsanulalifwan.barani.benchmark.rxjava.RxJavaBenchmark
+import com.app.ichsanulalifwan.barani.core.utils.toNewsEntity
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import org.junit.Test
 import org.junit.runner.RunWith
 
+@SuppressLint("CheckResult")
 @RunWith(AndroidJUnit4::class)
 class RxJavaIntegrationBenchmark : RxJavaBenchmark(), IntegrationBenchmark {
 
-    //    @Test
-//    override fun integration_1() = benchmarkRule.measureRepeated {
-//        runWithTimingDisabled { localSource.nukeAsCompletable().blockingAwait() }
-//        remoteSource.getTrendingNews()
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMapCompletable { repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }) }
-//            .blockingAwait()
-//    }
-//
-//    @Test
-//    override fun integration_2() = benchmarkRule.measureRepeated {
-//        runWithTimingDisabled { localSource.nukeAsCompletable().blockingAwait() }
-//        remoteSource.getTrendingNews()
-//            .flatMap { remoteSource.getTrendingNews() }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMapCompletable {
-//                repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() })
-//                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
-//            }.blockingAwait()
-//    }
-//
-//    @Test
-//    override fun integration_3() = benchmarkRule.measureRepeated {
-//        runWithTimingDisabled { localSource.nukeAsCompletable().blockingAwait() }
-//        remoteSource.getTrendingNews()
-//            .flatMap { remoteSource.getTrendingNews() }
-//            .flatMap { remoteSource.getTrendingNews() }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMapCompletable {
-//                repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() })
-//                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
-//                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
-//                    .andThen(Completable.complete())
-//            }.blockingAwait()
-//    }
-//
-//    @Test
-//    override fun integration_4() = benchmarkRule.measureRepeated {
-//        runWithTimingDisabled { localSource.nukeAsCompletable().blockingAwait() }
-//        remoteSource.getTrendingNews()
-//            .flatMap { remoteSource.getTrendingNews() }
-//            .flatMap { remoteSource.getTrendingNews() }
-//            .flatMap { remoteSource.getTrendingNews() }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMap { repository.getMoviesDetail(List(20) { it }) }
-//            .flatMapCompletable {
-//                repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() })
-//                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
-//                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
-//                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
-//                    .andThen(Completable.complete())
-//            }.blockingAwait()
-//    }
-//
-//    @Test
-//    override fun integration_reactive() = benchmarkRule.measureRepeated {
-//
-//        val flowable = Flowable.create<MovieResponse>({ emitter ->
-//            emitter.onNext(remoteSource.getTrendingNews().blockingGet())
-//        }, BackpressureStrategy.LATEST)
-//            .flatMapSingle { remoteSource.getNewsForRegion("PT") }
-//            .subscribeOn(Schedulers.io())
-//
-//        flowable.blockingFirst()
-//    }
-    override fun integration_1() {
-        TODO("Not yet implemented")
+    @Test
+    override fun integration1() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled { localDataSource.deleteNewsAsCompletable().blockingAwait() }
+        remoteDataSource.getTopHeadlines()
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMapCompletable { listResponse ->
+                repository.insertNewsToDatabase(
+                    listResponse.map { itemResponse ->
+                        itemResponse.toNewsEntity()
+                    }
+                )
+            }
+            .blockingAwait()
     }
 
-    override fun integration_2() {
-        TODO("Not yet implemented")
+    @Test
+    override fun integration2() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled { localDataSource.deleteNewsAsCompletable().blockingAwait() }
+        remoteDataSource.getTopHeadlines()
+            .flatMap { remoteDataSource.getTopHeadlines() }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMapCompletable { listResponse ->
+                repository.insertNewsToDatabase(
+                    listResponse.map { itemResponse ->
+                        itemResponse.toNewsEntity()
+                    }
+                )
+                    .andThen(repository.insertNewsToDatabase(listResponse.map { itemResponse -> itemResponse.toNewsEntity() }))
+            }.blockingAwait()
     }
 
-    override fun integration_3() {
-        TODO("Not yet implemented")
+    @Test
+    override fun integration3() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled { localDataSource.deleteNewsAsCompletable().blockingAwait() }
+        remoteDataSource.getTopHeadlines()
+            .flatMap { remoteDataSource.getTopHeadlines() }
+            .flatMap { remoteDataSource.getTopHeadlines() }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMapCompletable { listResponse ->
+                repository.insertNewsToDatabase(
+                    listResponse.map { itemResponse ->
+                        itemResponse.toNewsEntity()
+                    }
+                )
+                    .andThen(repository.insertNewsToDatabase(listResponse.map { itemResponse -> itemResponse.toNewsEntity() }))
+                    .andThen(repository.insertNewsToDatabase(listResponse.map { itemResponse -> itemResponse.toNewsEntity() }))
+                    .andThen(Completable.complete())
+            }.blockingAwait()
     }
 
-    override fun integration_4() {
-        TODO("Not yet implemented")
+    @Test
+    override fun integration4() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled { localDataSource.deleteNewsAsCompletable().blockingAwait() }
+        remoteDataSource.getTopHeadlines()
+            .flatMap { remoteDataSource.getTopHeadlines() }
+            .flatMap { remoteDataSource.getTopHeadlines() }
+            .flatMap { remoteDataSource.getTopHeadlines() }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMap { repository.getEverythingNews(List(20) { "us" }) }
+            .flatMapCompletable { listResponse ->
+                repository.insertNewsToDatabase(listResponse.map { itemResponse -> itemResponse.toNewsEntity() })
+                    .andThen(repository.insertNewsToDatabase(listResponse.map { itemResponse -> itemResponse.toNewsEntity() }))
+                    .andThen(repository.insertNewsToDatabase(listResponse.map { itemResponse -> itemResponse.toNewsEntity() }))
+                    .andThen(repository.insertNewsToDatabase(listResponse.map { itemResponse -> itemResponse.toNewsEntity() }))
+                    .andThen(Completable.complete())
+            }.blockingAwait()
     }
 
-    override fun integration_reactive() {
-        TODO("Not yet implemented")
+    @Test
+    override fun integrationReactive() = benchmarkRule.measureRepeated {
+
+        val flowable = Flowable.create({ emitter ->
+            emitter.onNext(remoteDataSource.getTopHeadlines().blockingGet())
+        }, BackpressureStrategy.LATEST)
+            .flatMapSingle { remoteDataSource.getNewsPublishers() }
+            .subscribeOn(Schedulers.io())
+
+        flowable.blockingFirst()
     }
 }
