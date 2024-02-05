87f047b22276cfa08faafed8b3550d1e5e8c5fcb
ichsanul.alifwan <ichsanulalifwan@gmail.com>
2024-02-05 23:11:36 +0800

    improve API config

--- core/src/main/java/com/app/ichsanulalifwan/barani/core/data/source/remote/network/ApiConfig.kt
+++ core/src/main/java/com/app/ichsanulalifwan/barani/core/data/source/remote/network/ApiConfig.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.app.ichsanulalifwan.barani.core.data.source.remote.network.rxjava.RxJavaNewsApiService
 import com.app.ichsanulalifwan.barani.core.mock.MockInterceptor
 import com.app.ichsanulalifwan.barani.core.utils.Constant.BASE_URL
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,40 +14,38 @@ import java.util.concurrent.TimeUnit
 
 object ApiConfig {
 
-    fun getApiService(): RxJavaNewsApiService {
-        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private fun createHttpClient(interceptor: Interceptor? = null): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().setLevel(
+            HttpLoggingInterceptor.Level.BODY
+        )
 
-        val client = OkHttpClient.Builder()
+        return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .apply { interceptor?.let { addInterceptor(it) } }
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
             .build()
+    }
 
-        val retrofit = Retrofit.Builder()
+    private fun createRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
             .client(client)
             .build()
+    }
 
+    fun getApiService(): RxJavaNewsApiService {
+        val client = createHttpClient()
+        val retrofit = createRetrofit(client)
         return retrofit.create(RxJavaNewsApiService::class.java)
     }
 
     fun getMockApiService(context: Context): RxJavaNewsApiService {
-
         val mockInterceptor = MockInterceptor(context = context)
-
-        val mockClient = OkHttpClient.Builder()
-            .addInterceptor(mockInterceptor)
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
-            .client(mockClient)
-            .build()
-
+        val client = createHttpClient(mockInterceptor)
+        val retrofit = createRetrofit(client)
         return retrofit.create(RxJavaNewsApiService::class.java)
     }
 
