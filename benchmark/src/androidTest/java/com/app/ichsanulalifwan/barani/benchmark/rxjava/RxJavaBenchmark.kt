40568ca48a6028a64eef26f32b21d65ec2b651e8
ichsanul.alifwan <ichsanulalifwan@gmail.com>
2024-02-05 23:31:03 +0800

    improve RxJavaBenchmark

--- benchmark/src/androidTest/java/com/app/ichsanulalifwan/barani/benchmark/rxjava/RxJavaBenchmark.kt
+++ benchmark/src/androidTest/java/com/app/ichsanulalifwan/barani/benchmark/rxjava/RxJavaBenchmark.kt
@@ -3,18 +3,29 @@ package com.app.ichsanulalifwan.barani.benchmark.rxjava
 import android.content.Context
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.test.platform.app.InstrumentationRegistry
-import com.app.ichsanulalifwan.barani.core.data.repository.news.NewsRepository
+import com.app.ichsanulalifwan.barani.core.data.repository.news.rxjava.RxJavaNewsRepository
 import com.app.ichsanulalifwan.barani.core.data.source.local.room.AppDatabase
+import com.app.ichsanulalifwan.barani.core.data.source.local.room.NewsDao
 import com.app.ichsanulalifwan.barani.core.data.source.remote.network.ApiConfig
+import com.app.ichsanulalifwan.barani.core.data.source.remote.network.rxjava.RxJavaNewsApiService
 import org.junit.Rule
 
 open class RxJavaBenchmark {
 
-    @Rule
+    protected val remoteDataSource: RxJavaNewsApiService by lazy {
+        ApiConfig.getMockApiService(context = context)
+    }
+
+    protected val localDataSource: NewsDao by lazy {
+        AppDatabase.getInstance(context = context).newsDao()
+    }
+
+    @get:Rule
     val benchmarkRule = BenchmarkRule()
 
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
-    protected val remoteDataSource = ApiConfig.getMockApiService()
-    protected val localDataSource = AppDatabase.getInstance(context).newsDao()
-    protected val repository = NewsRepository(remoteDataSource, localDataSource)
+    protected val repository = RxJavaNewsRepository(
+        remoteDataSource = remoteDataSource,
+        localDataSource = localDataSource,
+    )
 }
