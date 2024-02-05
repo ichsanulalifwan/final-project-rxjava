78bbeacf71696780c9a60f9d930528732c64e673
ichsanul.alifwan <ichsanulalifwan@gmail.com>
2024-02-06 01:18:21 +0800

    add constant

--- core/src/main/java/com/app/ichsanulalifwan/barani/core/utils/Constant.kt
+++ core/src/main/java/com/app/ichsanulalifwan/barani/core/utils/Constant.kt
@@ -6,4 +6,6 @@ object Constant {
     internal const val TOP_HEADLINES_URL = "top-headlines"
     internal const val EVERYTHING_URL = "everything"
     internal const val SOURCES_URL = "sources"
+    internal const val US_COUNTRY_CODE = "us"
+    internal const val HEALTH_CATEGORY = "health"
 }
